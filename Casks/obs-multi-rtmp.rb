ff688bad9f2fb45fcd4893aaa4e91649b918e249
Jat <jat@sinosky.org>
2023-11-23 20:32:02 +0800

    obs-multi-rtmp
    Signed-off-by: Jat <jat@sinosky.org>

---
+++ Casks/obs-multi-rtmp.rb
@@ -0,0 +1,20 @@
+cask 'obs-multi-rtmp' do
+  version '0.5.0.4'
+  sha256 '76b1cdae5ed9328dc216e38ccf1258f02dc647c03bdab6dd57cf255e6dece879'
+
+  url "https://github.com/sorayuki/obs-multi-rtmp/releases/download/#{version}/obs-multi-rtmp-#{version}-macos-universal.pkg"
+  name 'obs-multi-rtmp'
+  desc 'Multiple RTMP outputs plugin'
+  homepage 'https://github.com/sorayuki/obs-multi-rtmp'
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  pkg "obs-multi-rtmp-#{version}-macos-universal.pkg"
+
+  uninstall pkgutil: [
+    'com.sorayuki.obs-multi-rtmp'
+  ]
+end
